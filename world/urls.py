e81fb9baad667a1477ac47df6d0730844bffff9b
artneto <arthur_neto@icloud.com>
2022-02-21 20:28:41 +0000

    create the delete button and add order descending for my post

--- world/urls.py
+++ world/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import IndexView, ArticleView, PostView, UpdateViewPost
+from .views import IndexView, ArticleView, PostView, UpdateViewPost, DeleteViewPost
 
 urlpatterns = [
     path('', IndexView.as_view(), name="index"),
     path('article/<int:pk>', ArticleView.as_view(), name='details-article'),
     path('post/', PostView.as_view(), name='post'),
-    path('article/edit/<int:pk>', UpdateViewPost.as_view(), name='update_post')
+    path('article/edit/<int:pk>', UpdateViewPost.as_view(), name='update_post'),
+    path('article/<int:pk>/delete', DeleteViewPost.as_view(), name='post_delete'),
 ]
--- world/views.py
+++ world/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
-from django.views.generic import ListView, DetailView, CreateView, UpdateView
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from .models import Post
 from .forms import PostForm, EditForm
 from django import forms
+from django.urls import reverse_lazy
 
 # Create your views here.
 # Function django views ListView will list the posts from the blog
@@ -11,6 +12,7 @@ from django import forms
 class IndexView(ListView):
     model = Post
     template_name = 'index.html'
+    ordering = ['-id']
 
 
 class ArticleView(DetailView):
@@ -28,4 +30,9 @@ class UpdateViewPost(UpdateView):
     model = Post
     form_class = EditForm
     template_name = 'post_update.html'
-    #fields = ['title', 'title_tag', 'body']
+
+
+class DeleteViewPost(DeleteView):
+    model = Post
+    template_name = 'post_delete.html'
+    success_url = reverse_lazy('index')
