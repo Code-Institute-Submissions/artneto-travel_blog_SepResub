66e146346968faf1484deb44a0b93d1c161c3607
artneto <arthur_neto@icloud.com>
2022-03-05 15:06:23 +0000

    creaye edit profile page

--- members_signup/views.py
+++ members_signup/views.py
@@ -1,10 +1,21 @@
 from django.shortcuts import render
 from django.views import generic
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm
 from django.urls import reverse_lazy
 from .forms import SignUpForm
-#Create the Registration form
+
+#Create the Registration form using to sign up
 class UserRegisterView(generic.CreateView):
     form_class = SignUpForm
     template_name = 'registration/register.html'
     success_url = reverse_lazy('login')
+
+#Edit/update Profile Page
+class UserEditView(generic.UpdateView):
+    form_class = UserChangeForm
+    template_name = 'registration/edit_profile.html'
+    success_url = reverse_lazy('index')
+
+#function to display user who is updating his details. 
+    def get_object(self):
+        return self.request.user
