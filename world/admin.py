f5292efa3464d06cfea619a69557c4991985b8ee
artneto <arthur_neto@icloud.com>
2022-03-06 18:57:46 +0000

    create function to hold picture from profile and link to social media

--- world/admin.py
+++ world/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Post, Category
+from .models import Post, Category, Profile
 
-#this function allows the post entries to be accessible from the main area
+#This function allows the post entries to be accessible from the main area
 admin.site.register(Post)
 admin.site.register(Category)
+admin.site.register(Profile)
