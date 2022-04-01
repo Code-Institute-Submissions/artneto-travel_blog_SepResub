6a2d09249262553218a00feb960ce004a7f586f8
artneto <arthur_neto@icloud.com>
2022-04-01 07:47:05 +0000

    remove secret key

--- django_blog_travel/settings.py
+++ django_blog_travel/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY','django-insecure-++s(n)+4&&zvgsq$d8p@v3q!zf#ho(*ch60oboevm8e7yi71*+')
+SECRET_KEY = os.environ.get('SECRET_KEY','')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = development
