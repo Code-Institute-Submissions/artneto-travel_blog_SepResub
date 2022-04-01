ce0f8333281d895c61857f23e317d0a0d79f4d62
artneto <arthur_neto@icloud.com>
2022-04-01 16:15:42 +0000

    setting up deployment

--- django_blog_travel/settings.py
+++ django_blog_travel/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 import os
 import dj_database_url
 from pathlib import Path
+
 if os.path.isfile('env.py'):
     import env
 
@@ -35,7 +36,7 @@ DEBUG = development
 if development:
     ALLOWED_HOSTS = ['blogtravel-django.herokuapp.com', 'localhost']
 else:
-    ALLOWED_HOSTS = [os.environ.get('HEROKU_HOSTNAME')]
+    ALLOWED_HOSTS = [os.environ.get('blogtravel-django')]
 
 
 # Application definition
@@ -139,12 +140,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_STORAGE = 'cloudinary_storage.storage.StaticHashedCloudinaryStorage'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 MEDIA_URL = '/media/'
 DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 
 STATICFILES_DIRS = (
