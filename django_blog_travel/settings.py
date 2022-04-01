b060329a085518ddaff467437cab46dc267b6150
artneto <arthur_neto@icloud.com>
2022-04-01 08:34:20 +0000

    deployment commit

--- django_blog_travel/settings.py
+++ django_blog_travel/settings.py
@@ -13,11 +13,14 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 import os
 import dj_database_url
 from pathlib import Path
+if os.path.isfile('env.py'):
+    import env
 
 development = os.environ.get('DEVELOPMENT', False)
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 
 
 # Quick-start development settings - unsuitable for production
@@ -30,7 +33,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY','')
 DEBUG = development
 
 if development:
-    ALLOWED_HOSTS = ['localhost']
+    ALLOWED_HOSTS = ['blogtravel-django.herokuapp.com', 'localhost']
 else:
     ALLOWED_HOSTS = [os.environ.get('HEROKU_HOSTNAME')]
 
@@ -44,6 +47,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'cloudinary',
     'world',
     'members_signup',
     'ckeditor',
@@ -64,7 +68,7 @@ ROOT_URLCONF = 'django_blog_travel.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [TEMPLATES_DIR],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -133,13 +137,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_STORAGE = 'cloudinary_storage.storage.StaticHashedCloudinaryStorage'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 MEDIA_URL = '/media/'
+DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static')
-]
-
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
---
+++ env.py
@@ -0,0 +1,5 @@
+import os
+
+os.environ["DATABASE_URL"] = "postgres://wsqbovpkpfgupc:84c0a0bbdd720d8ca81ea2801f86f599bab1b8deff97c47f361de34cb42205c3@ec2-34-246-227-219.eu-west-1.compute.amazonaws.com:5432/dfeqhhh7gn4g7m"
+os.environ["SECRET_KEY"] = "2^aw%i=4le15d2y*-34=-2=tuy^6_&w9b!8dhfppu5q0csop4r"
+os.environ["CLOUDINARY_URL"] = "cloudinary://222547951187176:72QsdrJFQgtTvxTG9moEjgJTpLE@dlv5vjrep"
