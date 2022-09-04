bc0e456f4198d70c635a3a58ef8dce2ea597f97b
artneto <arthur_neto@icloud.com>
2022-09-04 08:42:25 +0000

    hidding env.py

--- env.py
+++
@@ -1,5 +0,0 @@
-import os
-
-os.environ["DATABASE_URL"] = "postgres://tpkribexetrzkx:d99d1f45c89132e5edb0a8fabcbe1e59b0fe58e6b3b854133d95713b237791e7@ec2-34-246-227-219.eu-west-1.compute.amazonaws.com:5432/d7fde63dmuuvlu"
-os.environ["SECRET_KEY"] = "2^aw%i=4le15d2y*-34=-2=tuy^6_&w9b!8dhfppu5q0csop4r"
-os.environ["CLOUDINARY_URL"] = "cloudinary://222547951187176:72QsdrJFQgtTvxTG9moEjgJTpLE@dlv5vjrep"
